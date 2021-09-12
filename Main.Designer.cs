aa4310740293f08c6e2ba1e689bfebdd30ce97e6
Logan <xackery@gmail.com>
2021-09-12 07:20:41 -0700

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace todo
+{
+    partial class Main
+    {
+       
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SysTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(2, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 150);
+            this.textBox1.TabIndex = 0;
+            // 
+            // SysTray
+            // 
+            this.SysTray.Icon = ((System.Drawing.Icon)(resources.GetObject("SysTray.Icon")));
+            this.SysTray.Text = "TODO";
+            this.SysTray.Visible = true;
+            this.SysTray.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SysTray_MouseDoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(200, 150);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "TODO";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon SysTray;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace todo
+{
+    
+    public partial class Main : Form
+    {
+        public const int WS_EX_STYLE = -20;
+        public const uint WS_EX_LAYERED = 0x00080000;
+        public const uint WS_EX_TRANSPARENT = 0x00000020;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern uint SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+        [DllImport("dwmapi.dll")]
+        public static extern void DwmExtendFrameIntoClientArea(IntPtr hWnd, ref int[] pMargins);
+
+        bool isOverlayMode = false;
+
+        public Main() {
+            //MessageBox.Show($"{GetWindowLong(this.Handle, -20)}");
+            InitializeComponent();
+            Hide();
+            SetOverlayMode(true);
+        }
+
+        private void SysTray_MouseDoubleClick(object sender, MouseEventArgs e) {
+            SetOverlayMode(!isOverlayMode);            
+        }
+
+        private void Main_Resize(object sender, EventArgs e) {
+        }
+
+        private void SetOverlayMode(bool value) {
+            isOverlayMode = value;
+
+            //sysTray.Text = $"TODO Overlay {(isOverlayMode ? "ON" : "OFF")}";
+            uint exStyle = GetWindowLong(this.Handle, WS_EX_STYLE);
+            if (isOverlayMode) {                
+                FormBorderStyle = FormBorderStyle.None;
+                Opacity = 0.5f;
+                SetWindowLong(Handle, WS_EX_STYLE, exStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);                
+                return;
+            }
+            FormBorderStyle = FormBorderStyle.Sizable;
+            Opacity = 1f;
+            SetWindowLong(Handle, WS_EX_STYLE, exStyle & ~(WS_EX_LAYERED | WS_EX_TRANSPARENT));
+            
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
